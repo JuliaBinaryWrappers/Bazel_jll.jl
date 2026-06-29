d146d34a58400dea739b5a23d171e3d21d6b06d1
jlbuild <juliabuildbot@gmail.com>
2026-06-29 16:32:14 +0000

    Bazel_jll build 8.7.0+0

--- src/Bazel_jll.jl
+++ src/Bazel_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Bazel")
-JLLWrappers.@generate_main_file("Bazel", UUID("102a56d7-9562-5839-a7f3-b61deef4aa17"))
+JLLWrappers.@generate_main_file("Bazel", Base.UUID("102a56d7-9562-5839-a7f3-b61deef4aa17"))
 end  # module Bazel_jll
